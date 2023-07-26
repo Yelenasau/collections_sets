7fca1ad2c8ec26311d72017cf4d13847e61976e9
Yelenasau <elenasauschkina@mail.ru>
2023-07-26 15:39:08 +0400

    2

---
+++ src/test/java/com/example/collections_sets/CollectionsSetsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.collections_sets;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CollectionsSetsApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
